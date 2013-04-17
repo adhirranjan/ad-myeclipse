7a3ddd7038ca01e89542338f19c38c3f0a9cbef9
adhirranjan@gmail.com <adhirranjan@gmail.com@a74eb804-93d9-643b-955a-23720e424168>
2013-04-17 06:02:37 +0000

    .

---  ad-myeclipse/songaa/Server/docs/pr_getSongUpdates.sql
+++  ad-myeclipse/songaa/Server/docs/pr_getSongUpdates.sql
@@ -6,7 +6,13 @@ CREATE PROCEDURE pr_getSongUpdates(
 )
 	TOP: BEGIN		
 		#song_url, image_url, user name, song desc, time stamp
-		select s.song_url, u.username, coalesce(s.description, '') description, s.image_url, s.ts
+		select s.song_url, u.username, coalesce(s.description, '') description, coalesce(s.image_url,'') image_url
+			,CASE
+				WHEN s.ts between date_sub(now(), INTERVAL 60 minute) and now() THEN concat(minute(TIMEDIFF(now(), s.ts)), ' minutes ago')
+				WHEN datediff(now(), s.ts) = 1 THEN 'Yesterday'
+				WHEN s.ts between date_sub(now(), INTERVAL 24 hour) and now() THEN concat(hour(TIMEDIFF(NOW(), s.ts)), ' hours ago')
+				ELSE date_format(s.ts, '%a, %m/%d/%y')
+				END 'ts'
 		from songs  s
 		inner join users u on u.user_id = s.user_id_fk
 		where user_id_fk <> p_userid;
@@ -20,7 +26,7 @@ call pr_getSongUpdates(1, @pout);
 select 1, @pout;
  */
 set @pout  = '';
-call pr_getSongUpdates(3, @pout);
+call pr_getSongUpdates(1, @pout);
 #select @pout ;
 
 
