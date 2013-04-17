d1b1a925938b83686338a29667045c6c8100d8f8
adhirranjan@gmail.com <adhirranjan@gmail.com@a74eb804-93d9-643b-955a-23720e424168>
2013-04-17 04:19:18 +0000

    .

---  ad-myeclipse/songaa/Server/BusinessLogic/SongBllc.java
+++  ad-myeclipse/songaa/Server/BusinessLogic/SongBllc.java
@@ -9,21 +9,21 @@ public class SongBllc {
 		String res = "";
 		try {
 			Connection con = DbContext.getConnection();
+
 			CallableStatement cs = null;
 			cs = con.prepareCall("{call pr_myfav(?,?)}");
 			cs.setString("p_userid", user_id); //
 			cs.registerOutParameter("p_out_error", Types.VARCHAR);
 
 			ResultSet rs = cs.executeQuery();
+
 			System.out.println("p_out_error: " + cs.getString("p_out_error"));
+
 			if (cs.getString("p_out_error") != null) {
 				res = String.format("{'error':'%s'}",
 						cs.getString("p_out_error"));
 			} else {
 				res = DbContext.resultSetToJson(rs);
-				// rs.next();
-				// res = String.format("{'user_id':'%s', 'name':'%s'}",
-				// rs.getString("user_id"), rs.getString("username"));
 			}
 
 		} catch (SQLException sqlex) {
@@ -38,4 +38,36 @@ public class SongBllc {
 		return res;
 	}
 
+	public static String getSongUpdates(String user_id) {
+		String res = "";
+		try {
+			Connection con = DbContext.getConnection();
+
+			CallableStatement cs = null;
+			cs = con.prepareCall("{call pr_getSongUpdates(?,?)}");
+			cs.setString("p_userid", user_id); //
+			cs.registerOutParameter("p_out_error", Types.VARCHAR);
+
+			ResultSet rs = cs.executeQuery();
+
+			System.out.println("p_out_error: " + cs.getString("p_out_error"));
+
+			if (cs.getString("p_out_error") != null) {
+				res = String.format("{'error':'%s'}",
+						cs.getString("p_out_error"));
+			} else {
+				res = DbContext.resultSetToJson(rs);
+			}
+
+		} catch (SQLException sqlex) {
+			res = String.format("{'error':'%s'}", sqlex.getMessage());
+			System.out.println("SQLException: " + sqlex.getMessage()
+					+ sqlex.getSQLState());
+		} catch (Exception ex) {
+			res = String.format("{'error':'%s'}", ex.getMessage());
+			System.out.println(ex.getMessage());
+		}
+		System.out.println("res: " + res);
+		return res;
+	}
 }
